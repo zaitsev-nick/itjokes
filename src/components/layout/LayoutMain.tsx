8b1cc027ba60597865395da6753d5bcd52cb504d
Nick Zaitsau <nickzcv@gmail.com>
2023-12-24 00:42:07 +0300

    init

---
+++ src/components/layout/LayoutMain.tsx
@@ -0,0 +1,16 @@
+import type { PropsWithChildren } from 'react';
+import { Header } from './header/Header';
+import styles from './LayoutMain.module.scss';
+
+
+export default function LayoutMain({ children }:
+  PropsWithChildren<unknown>) {
+  return (
+    <>
+      <Header />
+      <main className={styles.layout}>
+        <div>{children}</div>
+      </main>
+    </>
+  )
+}
