aa56a275e7c037cc69f7edfb0032c03370da6dd9
Nick Zaitsau <nickzcv@gmail.com>
2023-12-25 21:36:07 +0300

    add loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,29 @@ import { useEffect, useState } from 'react';
 
 export default function Home() {
   const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     fetch('http://localhost:3000/api/jokes')
     .then((res) => res.json())
-    .then((data) => setData(data))
+    .then((data) => {
+      setData(data);
+      setLoading(false);
+    })
   }, []);
 
   return (
     <div className="p-5 sm:p-8">
+      {loading ? (
+        <div className="inline-block h-10 w-10 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" role="status">
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
+        </div>
+      ) : (
         <div className="columns-1 gap-5 sm:columns-2 sm:gap-8 md:columns-3 lg:columns-4 [&>img:not(:first-child)]:mt-8">
             {data && data.map((item) => <img src={item.image_url} key={item.id}></img>)}
         </div>
+      )}
     </div>
   )
 }
