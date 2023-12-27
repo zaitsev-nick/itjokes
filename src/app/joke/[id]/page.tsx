c003b60e73f135cd7b705674c336ff5222db1c42
Nick Zaitsau <nickzcv@gmail.com>
2023-12-27 20:54:17 +0300

    revert last changes

--- src/app/joke/[id]/page.tsx
+++ src/app/joke/[id]/page.tsx
@@ -1,18 +1,12 @@
 import { Metadata } from 'next';
 import { JokeItem } from '@/components/layout/joke/JokeItem';
-import { db } from '@/lib/db';
 
 //  Return a list of `params` to populate the [id] dynamic segment
 export async function generateStaticParams() {
-  //const jokes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/jokes`).then((res) => res.json());
-  const jokes = await db.jokes.findMany({
-    orderBy: {
-      id: 'desc',
-    }
-  })
+  const jokes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/jokes`).then((res) => res.json());
+ 
   return jokes.map((joke: { id: number }) => ({
     id: `${joke.id}`,
-    fallback: true,
   }));
 }
 
