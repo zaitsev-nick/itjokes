56e37d8e31bd35a23f9a367158b2a39e754f56e7
Nick Zaitsau <nickzcv@gmail.com>
2023-12-27 20:45:18 +0300

    try to solve build

--- src/app/joke/[id]/page.tsx
+++ src/app/joke/[id]/page.tsx
@@ -1,12 +1,18 @@
 import { Metadata } from 'next';
 import { JokeItem } from '@/components/layout/joke/JokeItem';
+import { db } from '@/lib/db';
 
 //  Return a list of `params` to populate the [id] dynamic segment
 export async function generateStaticParams() {
-  const jokes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/jokes`).then((res) => res.json());
- 
+  //const jokes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/jokes`).then((res) => res.json());
+  const jokes = await db.jokes.findMany({
+    orderBy: {
+      id: 'desc',
+    }
+  })
   return jokes.map((joke: { id: number }) => ({
     id: `${joke.id}`,
+    fallback: true,
   }));
 }
 
